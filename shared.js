79decd55f7488644945b37ac07e6170c0cb213a5
manu <manu.villar.vitoria@gmail.com>
2020-07-26 20:59:26 +0200

    adding Mobile Navbar to the Customers & Packages pages

--- shared.js
+++ shared.js
@@ -19,10 +19,14 @@ backdrop.addEventListener("click", () => {
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if(modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
-    modal.classList.remove('open');
+    if(modal) {
+        modal.classList.remove('open');
+    }
     backdrop.classList.remove('open');
 }
 
